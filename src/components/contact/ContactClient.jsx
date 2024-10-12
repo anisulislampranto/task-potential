c7696989e91eac0a80180daaf431be170ed5227f
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2024-10-12 17:06:07 +0600

    Update ContactClient.jsx using wrap

--- src/components/contact/ContactClient.jsx
+++ src/components/contact/ContactClient.jsx
@@ -11,7 +11,7 @@ export default function ContactClient() {
     <div className=' dark:bg-[#1e1e1e] py-20 space-y-10 px-10'>
         <HeadingDescription className={'text-center max-w-3xl mx-auto'} heading={contactData.heading} description={contactData.description} />
 
-        <form action="" className='flex justify-center items-center gap-4 font-poppinsRegular'>
+        <form action="" className='flex flex-wrap justify-center items-center gap-4 font-poppinsRegular'>
             <input type="text" placeholder='Enter Your Email' className='w-[20rem] md:w-[30rem] bg-[#f8f8f8] rounded-md outline-none border p-3' />
             <input type="button" value="Contact Me" className=' text-white bg-[#fd6f00] p-3 rounded-md' />
         </form>
